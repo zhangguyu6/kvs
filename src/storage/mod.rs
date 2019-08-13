0e4b65a12ead732e17ebaf20f45e3682d0cbcfaa
Zhang Guyu <zhangguyu6@gmail.com>
2019-08-13 19:32:15 +0800

    Update mod.rs

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -3,15 +3,25 @@ mod io;
 // mod layout;
 // mod segement;
 
-pub use io::{BlockDev, RawBlockDev};
+pub use io::{BlockDev, RawBlockDev,Dummy};
 pub use block_allocater::BlockAllocater;
 
 pub const BLOCK_SIZE: usize = 4096;
 pub type BlockId = u32;
 
-#[derive(Eq, PartialEq)]
+#[derive(Eq, PartialEq,Clone)]
 pub struct ObjectPos {
     pub block_start: BlockId,
     pub block_len: u16,
     pub offset: u16,
+}
+
+impl Default for ObjectPos {
+    fn default() -> Self {
+        Self {
+            block_start:0,
+            block_len:0,
+            offset:0
+        }
+    }
 }
