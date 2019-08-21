1d151ab31461edacebb6d588ae27414c51c340a0
Zhang Guyu <zhangguyu6@gmail.com>
2019-08-21 17:21:32 +0800

    Update mod.rs

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -3,16 +3,16 @@ mod dev;
 use crate::error::TdbError;
 pub use dev::{DataLogFile,MetaTableFile,MetaLogFile,Dev};
 pub use obj_pos::{ObjectPos,MAX_DATABASE_SIZE,MAX_OBJECT_SIZE};
-
+use std::io::{Read,Write};
 
 
  
 
 
 pub trait Serialize {
-    fn serialize(&self, writer: &mut [u8]) -> Result<(), TdbError>;
+    fn serialize<W:Write>(&self, writer: &mut W) -> Result<(), TdbError>;
 }
 
 pub trait Deserialize: Sized {
-    fn deserialize(reader: &[u8]) -> Result<Self, TdbError>;
+    fn deserialize<R:Read>(reader: &mut R) -> Result<Self, TdbError>;
 }
