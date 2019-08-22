5615d91304c0edf4a875f7900e4561551d3c3632
Zhang Guyu <zhangguyu6@gmail.com>
2019-08-22 20:07:14 +0800

    Update obj_pos.rs

--- src/storage/obj_pos.rs
+++ src/storage/obj_pos.rs
@@ -1,11 +1,11 @@
 use crate::object::ObjectTag;
 use std::io::SeekFrom;
-
+use std::u64;
 // [20~63)
 pub const MAX_DATABASE_SIZE: u64 = (1 << 44) - 1;
 // [4~20)
 pub const MAX_OBJECT_SIZE: u64 = (1 << 16) - 1;
-// [0~4)
+// [1~4)
 pub const MAX_OBJECT_TAG_SIZE: u64 = (1 << 4) - 1;
 
 #[derive(Eq, PartialEq, Clone, Debug)]
@@ -18,9 +18,9 @@ impl Into<SeekFrom> for ObjectPos {
 }
 
 impl ObjectPos {
-    pub fn new(pos: u64, len: usize, tag: ObjectTag) -> Self {
+    pub fn new(pos: u64, len: u16, tag: ObjectTag) -> Self {
         let tag: u8 = tag.into();
-        Self((pos << 20) + ((len as u64) << 4) + (tag as u64))
+        Self((pos << 20) + ((len as u64) << 4) + tag as u64)
     }
 
     #[inline]
@@ -29,13 +29,32 @@ impl ObjectPos {
     }
 
     #[inline]
-    pub fn get_len(&self) -> usize {
-        ((self.0 >> 4) & 0xfffff) as usize
+    pub fn get_len(&self) -> u16 {
+        ((self.0 >> 4) & 0xffff) as u16
+    }
+
+    #[inline]
+    pub fn add_len(&mut self, size: u16) -> u16 {
+        let new_len = ((self.0 >> 4) & 0xffff) as u16 + size;
+        self.0 = (self.0 & ((u64::MAX >> 20 << 20) + 0xf)) + ((new_len as u64) << 4);
+        new_len
+    }
+
+    #[inline]
+    pub fn sub_len(&mut self, size: u16) -> u16 {
+        let new_len = ((self.0 >> 4) & 0xffff) as u16 - size;
+        self.0 = (self.0 & ((u64::MAX >> 20 << 20) + 0xf)) + ((new_len as u64) << 4);
+        new_len
     }
 
     #[inline]
     pub fn get_tag(&self) -> ObjectTag {
         ObjectTag::from((self.0 & 0xf) as u8)
+    }
+
+    #[inline]
+    pub fn is_empty(&self) -> bool {
+        self.0 == 0
     }
 }
 
@@ -45,8 +64,29 @@ impl Default for ObjectPos {
     }
 }
 
-impl ObjectPos {
-    pub fn is_empty(&self) -> bool {
-        self.0 == 0
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::u16;
+    #[test]
+    fn test_obj_pos() {
+        let obj_pos = ObjectPos::default();
+        assert!(obj_pos.is_empty());
+        let mut obj_pos = ObjectPos::new(1, 127, ObjectTag::Branch);
+        assert_eq!(obj_pos.get_pos(), 1);
+        assert_eq!(obj_pos.get_len(), 127);
+        assert_eq!(obj_pos.get_tag(), ObjectTag::Branch);
+        obj_pos.add_len(1);
+        assert_eq!(obj_pos.get_len(), 128);
+        assert_eq!(obj_pos.get_pos(), 1);
+        assert_eq!(obj_pos.get_tag(), ObjectTag::Branch);
+        obj_pos.sub_len(2);
+        assert_eq!(obj_pos.get_len(), 126);
+        assert_eq!(obj_pos.get_pos(), 1);
+        assert_eq!(obj_pos.get_tag(), ObjectTag::Branch);
+        let obj_pos = ObjectPos::new(1 << 40, u16::MAX, ObjectTag::Leaf);
+        assert_eq!(obj_pos.get_pos(), 1 << 40);
+        assert_eq!(obj_pos.get_len(), u16::MAX);
+        assert_eq!(obj_pos.get_tag(), ObjectTag::Leaf);
     }
 }
