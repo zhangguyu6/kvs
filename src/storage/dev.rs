5ae31914d68ac698faa53d6f81079a5db9401925
Zhang Guyu <zhangguyu6@gmail.com>
2019-08-23 17:38:52 +0800

    Update dev.rs

--- src/storage/dev.rs
+++ src/storage/dev.rs
@@ -1,5 +1,5 @@
 use super::ObjectPos;
-use crate::meta::{CheckPoint, ObjectTablePage, OBJECT_TABLE_PAGE_SIZE};
+use crate::meta::{CheckPoint, ObjectTablePage, OBJECT_TABLE_PAGE_SIZE,PageId};
 use crate::storage::{Deserialize, Serialize};
 use crate::{
     error::TdbError,
@@ -9,7 +9,6 @@ use std::fs::{self, File};
 use std::io::{BufReader, BufWriter, Read, Seek, SeekFrom, Write};
 use std::path::{Path, PathBuf};
 
-pub const META_LOG_FILE_MAX_SIZE: usize = 1 << 21;
 
 pub struct Dev {
     pub dir_path: PathBuf,
@@ -54,102 +53,8 @@ impl Dev {
     // }
 }
 
-pub struct MetaTableFileWriter {
-    reader: BufReader<File>,
-    writer: BufWriter<File>,
-    size: usize,
-}
 
-impl MetaTableFileWriter {
-    pub fn read_page(&mut self, page_id: u32) -> Result<ObjectTablePage, TdbError> {
-        self.reader.seek(SeekFrom::Start(
-            page_id as u64 * OBJECT_TABLE_PAGE_SIZE as u64,
-        ))?;
-        ObjectTablePage::deserialize(&mut self.reader)
-    }
-    pub fn write_page(&mut self, page: ObjectTablePage) -> Result<(), TdbError> {
-        let page_id = page.get_page_id();
-        if page_id as usize * OBJECT_TABLE_PAGE_SIZE <= self.size {
-            self.writer.seek(SeekFrom::Start(
-                page_id as u64 * OBJECT_TABLE_PAGE_SIZE as u64,
-            ))?;
-        } else {
-            assert_eq!(
-                self.size + OBJECT_TABLE_PAGE_SIZE,
-                page_id as usize * OBJECT_TABLE_PAGE_SIZE
-            );
-            self.writer.seek(SeekFrom::Start(self.size as u64))?;
-        }
-        self.size += OBJECT_TABLE_PAGE_SIZE;
-        page.serialize(&mut self.writer)
-    }
-}
 
-pub struct MetaLogFileWriter {
-    writer: BufWriter<File>,
-    size: usize,
-}
-
-impl MetaLogFileWriter {
-    fn write_cp(&mut self, cp: &CheckPoint) -> Result<(), TdbError> {
-        if self.size + cp.len() > META_LOG_FILE_MAX_SIZE {
-            return Err(TdbError::NoSpace);
-        } else {
-            self.writer.seek(SeekFrom::Start(self.size as u64))?;
-            cp.serialize(&mut self.writer)?;
-            self.size += cp.len();
-            self.writer.flush()?;
-            Ok(())
-        }
-    }
-
-    fn write_cp_rename<P: AsRef<Path>>(
-        &mut self,
-        cp: &CheckPoint,
-        dir_path: P,
-    ) -> Result<MetaLogFileWriter, TdbError> {
-        let mut options = fs::OpenOptions::new();
-        let options_mut = options.create(true).read(true).write(true).append(true);
-        let mut path = PathBuf::from(dir_path.as_ref());
-        let mut old_path = path.clone();
-        old_path.push("meta_log_file.db");
-        path.push("meta_log_file_temp.db");
-        let file = options_mut.open(&path)?;
-        let mut writer = BufWriter::with_capacity(4096 * 4, file);
-        cp.serialize(&mut writer)?;
-        writer.flush()?;
-        fs::rename(&path, &old_path)?;
-        let writer = BufWriter::with_capacity(4096 * 2, options_mut.open(&old_path)?);
-        Ok(MetaLogFileWriter {
-            writer: writer,
-            size: cp.len(),
-        })
-    }
-}
-
-pub struct DataLogFileReader {
-    reader: BufReader<File>,
-}
-
-impl DataLogFileReader {
-    pub fn read_obj(&mut self, obj_pos: &ObjectPos) -> Result<Object, TdbError> {
-        self.reader.seek(obj_pos.clone().into())?;
-        let obj_tag = obj_pos.get_tag();
-        Object::read(&mut self.reader, &obj_tag)
-    }
-}
-
-pub struct DataLogFilwWriter {
-    writer: BufWriter<File>,
-}
-
-impl DataLogFilwWriter {
-    pub fn write_obj_log(&mut self, obj_log: &ObjectLog) -> Result<(), TdbError> {
-        obj_log.serialize(&mut self.writer)?;
-        self.writer.flush()?;
-        Ok(())
-    }
-}
 
 #[cfg(test)]
 mod tests {
