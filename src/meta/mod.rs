95a6dc1a8a5a80c0f0f9648c553411e9d4bf42a2
Zhang Guyu <zhangguyu6@gmail.com>
2019-08-28 17:46:51 +0800

    Update mod.rs

--- src/meta/mod.rs
+++ src/meta/mod.rs
@@ -1,15 +1,14 @@
 mod check_point;
+mod immut_table;
+mod mut_table;
 mod object_allocater;
 mod object_manager;
 mod object_table;
 mod table;
-mod immut_table;
-mod mut_table;
 pub use check_point::CheckPoint;
 pub use object_allocater::ObjectAllocater;
 pub use object_manager::{ObjectAccess, ObjectModify};
 pub use object_table::{
-    ObjectTable, ObjectTablePage, OBJECT_NUM, OBJECT_TABLE_ENTRY_PRE_PAGE,
-    OBJECT_TABLE_PAGE_SIZE,
+    ObjectTable, ObjectTablePage, OBJECT_NUM, OBJECT_TABLE_ENTRY_PRE_PAGE, OBJECT_TABLE_PAGE_SIZE,
 };
-pub use table::{InnerTable,PageId,OBJ_PRE_PAGE};
+pub use table::{InnerTable, PageId, MAX_PAGE_NUM, OBJ_PRE_PAGE};
