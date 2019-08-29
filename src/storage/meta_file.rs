f23eb4b0ba1abb368831d7dcbc7b9fa4aa66d9be
Zhang Guyu <zhangguyu6@gmail.com>
2019-08-29 20:19:34 +0800

    Update meta_file.rs

--- src/storage/meta_file.rs
+++ src/storage/meta_file.rs
@@ -1,14 +1,14 @@
 use crate::error::TdbError;
 use crate::meta::CheckPoint;
-use crate::storage::{Deserialize, Serialize, StaticSized};
+use crate::storage::{Deserialize, Serialize};
 use std::fs::{self, File};
-use std::io::{BufReader, BufWriter, Seek, SeekFrom, Write};
+use std::io::{BufReader, BufWriter, Write};
 use std::path::{Path, PathBuf};
 
 // meta log file less than 2M
 pub const META_LOG_FILE_MAX_SIZE: usize = 1 << 21;
 
-const DEFAULT_BUF_SIZE: usize = 4096 * 2;
+const DEFAULT_BUF_SIZE: usize = 4096;
 
 pub struct MetaLogFileWriter {
     writer: BufWriter<File>,
@@ -22,34 +22,42 @@ impl MetaLogFileWriter {
             size: size,
         }
     }
-    pub fn write_cp(&mut self, cp: &CheckPoint) -> Result<(), TdbError> {
-        if self.size + cp.len() > META_LOG_FILE_MAX_SIZE {
-            return Err(TdbError::NoSpace);
+    /// Write checkpoint to meta file
+    /// Return true if should apply
+    pub fn write_cp(&mut self, mut cp: CheckPoint) -> Result<bool, TdbError> {
+        self.size += cp.len();
+        cp.meta_size = self.size as u32;
+        cp.serialize(&mut self.writer)?;
+        self.writer.flush()?;
+        if self.size <= META_LOG_FILE_MAX_SIZE {
+            Ok(false)
         } else {
-            self.writer.seek(SeekFrom::Start(self.size as u64))?;
-            cp.serialize(&mut self.writer)?;
-            self.size += cp.len();
-            self.writer.flush()?;
-            Ok(())
+            Ok(true)
         }
     }
 
+    /// Write checkpoint to template meta file and rename template meta file to meta file
+    /// # Notes
+    /// checkpoint should be applied before write to template meta file
     pub fn write_cp_rename<P: AsRef<Path>>(
         &mut self,
-        cp: &CheckPoint,
+        mut cp: CheckPoint,
         meta_log_file_path: P,
     ) -> Result<(), TdbError> {
         let mut options = fs::OpenOptions::new();
-        let options_mut = options.create(true).read(true).write(true).append(true);
+        let options_mut = options.create(true).write(true).append(true);
         let mut temp_path = PathBuf::from(meta_log_file_path.as_ref());
         temp_path.pop();
         temp_path.push("meta_log_file_temp.db");
         let file = options_mut.open(&temp_path)?;
-        self.writer = BufWriter::with_capacity(4096 * 2, file);
-        self.size = 0;
-        self.write_cp(cp)?;
+        self.writer = BufWriter::with_capacity(DEFAULT_BUF_SIZE, file);
+        self.size = cp.len();
+        assert!(self.size < META_LOG_FILE_MAX_SIZE && cp.obj_changes.len() == 0);
+        cp.meta_size = self.size as u32;
+        cp.serialize(&mut self.writer)?;
+        self.writer.flush()?;
         fs::rename(&temp_path, &meta_log_file_path)?;
-        self.writer = BufWriter::with_capacity(4096 * 2, options_mut.open(&meta_log_file_path)?);
+        self.writer = BufWriter::with_capacity(DEFAULT_BUF_SIZE, options_mut.open(&meta_log_file_path)?);
         Ok(())
     }
 }
