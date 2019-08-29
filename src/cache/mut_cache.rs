aa425809093c3b926ccbe82f1073d06e64667e53
Zhang Guyu <zhangguyu6@gmail.com>
2019-08-29 20:17:21 +0800

    Update mut_cache.rs

--- src/cache/mut_cache.rs
+++ src/cache/mut_cache.rs
@@ -1,28 +1,37 @@
 use crate::object::{MutObject, Object, ObjectId};
-use std::collections::HashMap;
+use std::collections::{HashMap,hash_map::{IterMut,Iter}} ;
 
 pub struct MutCache {
     dirties: HashMap<ObjectId, MutObject>,
+    removed_size: u64,
 }
 
 impl Default for MutCache {
     fn default() -> Self {
         MutCache {
             dirties: HashMap::default(),
+            removed_size:0,
         }
     }
 }
 
-// Readonly in cache
-// New/Del/Dirty in dirties
-// There is no intersection between the dirties and cache
+/// Readonly in cache
+/// New/Del/Dirty in dirties
+/// There is no intersection between the dirties and cache
 impl MutCache {
-    //  Return true if oid in dirties
+    ///  Return true if oid in dirties
     pub fn contain(&mut self, oid: ObjectId) -> bool {
         self.dirties.contains_key(&oid)
     }
     pub fn remove(&mut self, oid: ObjectId) -> Option<MutObject> {
-        self.dirties.remove(&oid)
+        let mut_obj =  self.dirties.remove(&oid);
+        if let Some(mut_obj) = mut_obj {
+            // if object is on disk, Logically remove it and add remove size 
+            if mut_obj.is_readonly() {
+                self.removed_size += mut_obj.get_ref().unwrap().get_pos().get_len() as u64;
+            }
+        }
+        mut_obj
     }
     pub fn insert(&mut self, oid: ObjectId, obj_mut: MutObject) -> Option<MutObject> {
         self.dirties.insert(oid, obj_mut)
@@ -33,6 +42,8 @@ impl MutCache {
     pub fn get_mut_dirty(&mut self, oid: ObjectId) -> Option<&mut Object> {
         let obj_mut = self.dirties.remove(&oid)?;
         if obj_mut.is_readonly() {
+            // if object is on disk, Logically remove it and add remove size 
+            self.removed_size += obj_mut.get_ref().unwrap().get_pos().get_len() as u64;
             let obj_dirty = obj_mut.to_dirty();
             self.dirties.insert(oid, obj_dirty);
         } else {
@@ -44,6 +55,18 @@ impl MutCache {
         self.dirties.get(&oid)?.get_ref()
     }
     pub fn drain(&mut self) -> Vec<(ObjectId, MutObject)> {
+        self.removed_size = 0;
         self.dirties.drain().collect()
+    }
+    pub fn iter_mut(&mut self) -> IterMut<ObjectId, MutObject> {
+        self.dirties.iter_mut()
+    }
+
+    pub fn iter(&mut self) -> Iter<ObjectId, MutObject> {
+        self.dirties.iter()
+    }
+
+    pub fn get_removed_size(&self) -> u64 {
+        self.removed_size
     }
 }
